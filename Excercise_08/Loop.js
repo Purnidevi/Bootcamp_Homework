ee93ed6e375699f5789d9574dd562f4cd746dd38
Purnidevi <r.purnidevi.usa@gmail.com>
2024-02-20 20:59:48 -0500

    update

--- Excercise_08/Loop.js
+++ Excercise_08/Loop.js
@@ -1,34 +1,34 @@
 
-//   Using a loop, iterate through this array  and  console.log all of the people.
+  // Using a loop, iterate through this array  and  console.log all of the people.
 
 
+nameid = ["Grey","James"]
+let a = "Matt";
+let b = "Lill"
 
+console.log (nameid.splice(0,2)); // .splice([array number],ordernumber)//(0,1,2,3...),(1,2,3....)
+console.log(nameid)
+console.log (nameid.push(a,b)); //add
+console.log(nameid)
 
-// a = "Matt";
-// b = "Lill"
+let nameid1 = ["Grey","James","Matt","Lill"]
 
-// nameid = ["Grey","James"]
+console.log (nameid1.pop()); // remove last
+console.log(nameid1)
+console.log (nameid1.shift());// remove first
+console.log(nameid1)
 
-// console.log (nameid.splice(0,2)); // .splice([array number],ordernumber)//(0,1,2,3...),(1,2,3....)
-// console.log(nameid)
-// console.log (nameid.push(a,b)); //add
-// console.log(nameid)
-
-
-// console.log (nameid.pop()); // remove last
-// console.log(nameid)
-// console.log (nameid.shift());// remove first
-// console.log(nameid)
 
 // arrayOfLetters.slice(0, n).concat(arrayOfLetters.slice(n+1))
 
-// c =["Mary"];
-// d=["Matt"];
-// e=c.slice(0,1).concat(d.slice(0,1))
-// console.log(e)
+let c =["Emile"];
+let d =["Paige"];
+let g =["Lisa"];
+e = c.slice(0,1).concat(d.slice(0,1))
+console.log(e)
 
-// f= e.concat(d.slice(0,2))
-// console.log(f)
+f= e.concat(g.slice(0,2))
+console.log(f)
 
 //Loop
 n = ['Matt','lill','Mary','Grey','James']
@@ -39,8 +39,8 @@ for (i=0;i<n.length;i++){
     else console.log(n[i])
    }
 
-//
-s = ['purni','Matt','lill','Mary','Grey','James']
+
+let s = ['purni','Matt','lill','Mary','Grey','James']
 dummy = []
 for (i=0; i<s.length; i++ ) {
     
@@ -51,10 +51,24 @@ for (i=0; i<s.length; i++ ) {
 }
 console.log(dummy)
 
-b = ['purni','Matt','lill','Mary','Grey','James']
 
-console.log(b.indexOf('Matt'))
-console.log(b.indexOf('Mary'))
-console.log(b.with.call('Foo', -1,0))
+let h = ['purni','Matt','lill','Mary','Grey','James']
+
+console.log(h.indexOf('Matt'))
+console.log(h.indexOf('Mary'))
+console.log(h.indexOf('Foo'))
+
+console.log(h.with.call('Foo', -1,0))
+
+
+let y = ['Devon','Greg', "Mary", "James" ]
+let z =["Elizabeth", "Artie"]
+console.log(y.splice(0,1))
+console.log(y)
+console.log(y.concat(z.slice(0,2)))
+console.log(y)
+let k = ['Bob']
+console.log(y.concat(k.slice(0,1)))
+
 
 
--- Excercise_08/loop1.js
+++
@@ -1,9 +0,0 @@
-
-
-
-b = ['purni','Matt','lill','Mary','Grey','James']
-
-console.log(b.indexOf('Matt'))
-console.log(b.indexOf('Mary'))
-
-c = ['purni','Matt','lill','Mary','Grey','James']
